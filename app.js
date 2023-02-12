f35cb1cd19b265242dd133432fe82dd2d8700a8b
Armin Hossein Zadeh <arminfrontend@gmail.com>
2023-02-12 23:00:08 +0330

    Feat: Toggled the mobile menu

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const menuBtn = document.getElementById("menu-btn")
+const mobileMenu = document.getElementById("mobile-menu")
 
 const toggleBtn = () => {
     menuBtn.classList.toggle("open")
+    mobileMenu.classList.toggle("flex")
+    mobileMenu.classList.toggle("hidden")
+
 }
 
-menuBtn.addEventListener("click" , toggleBtn)
-
+menuBtn.addEventListener("click", toggleBtn)
